d784b8263e7b8aaea06398f7f66d4dbecfb9db2e
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-20 14:42:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all.clear
+  end
+
+  def self.print_all(dogs)
+     dogs.each do |name|
+     puts "#{name}"
+   end
+ end
+   
+   def save
+     @save << @@all
+   end
+   
+   def clear_all
+     clear(@@all)
+   end
+    
+end
